d5461c25db63e70cc7d7cb2df61e934bd14b1e55
ql <94349690+adbackend@users.noreply.github.com>
2022-01-19 20:49:11 +0900

    Update LoginMapper.java

--- src/main/java/kr/spring/login/dao/LoginMapper.java
+++ src/main/java/kr/spring/login/dao/LoginMapper.java
@@ -2,12 +2,7 @@ package kr.spring.login.dao;
 
 import kr.spring.login.vo.LoginVO;
 
-/**
-  * @FileName : LoginMapper.java
-  * @Date : 2021. 10. 9. 
-  * @Author : 최유정
-  * @Description : LoginMapper
-  */
+
 public interface LoginMapper {
 	
 	public LoginVO loginAction(String id);
